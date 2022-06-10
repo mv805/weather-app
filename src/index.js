f5a5ca044e3a29008cd860a61d785ebafd0e3a36
Matt Villa <matthewvilla805@gmail.com>
2022-06-10 00:50:42 -0700

    Added some notes for required attributes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 
 /* free API key: d78f36805db656f7f4cf2a5ddb1892c7
-*
-*
-*
-*
-*
+* temperature, feels like, high, low
+* pressure, humidity
+* wind speed, angle
+* description "cloudy, clear sky etc."
+* city name
 *
 *
 */
