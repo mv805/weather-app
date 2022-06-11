f7ef531505dcf0d0be352404455291a224c6f389
Matt Villa <matthewvilla805@gmail.com>
2022-06-11 00:18:21 -0700

    removed notes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
+import * as pageBuilder from './display.js';
 import './style.css';
 
+
 /* free API key: d78f36805db656f7f4cf2a5ddb1892c7
-* temperature, feels like, high, low
-* pressure, humidity
-* wind speed, angle
-* description "cloudy, clear sky etc."
-* city name
-*
-*
+
 */
+pageBuilder.createSearchBarArea();
