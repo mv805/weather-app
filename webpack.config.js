a5616967aeb6dee371fc1f7fb1062e8283e92f47
Matt Villa <matthewvilla805@gmail.com>
2022-06-10 22:42:58 -0700

    svg rules added

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.svg/,
+                type: 'asset/inline',
+            },
         ],
     },
 };
